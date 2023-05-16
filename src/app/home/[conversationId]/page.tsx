2e9781594a7d90f2d2d39f6f9ce478c18fa3e9fe
0AliReza0 <alireza.abedi9310@gmail.com>
2023-05-16 22:30:04 +0430

    chat section added...

--- src/app/home/[conversationId]/page.tsx
+++ src/app/home/[conversationId]/page.tsx
@@ -1,20 +1,4 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-
 const ConversationChat = () => {
-  const [isInSlug, setIsInSlug] = useState<boolean>(false);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname.split("/")[2]) {
-      setIsInSlug(true);
-    } else {
-      setIsInSlug(false);
-    }
-  }, [pathname]);
-
   return (
     <section className="flex flex-col justify-start items-center col-span-3 h-full">
       {/* Header */}
