dbd764b9e417f422f7ecbeb996eae6ecadfe058c
0AliReza0 <alireza.abedi9310@gmail.com>
2023-05-28 16:01:16 +0430

    commenting...

--- src/server/utils/verifyTokens.ts
+++ src/server/utils/verifyTokens.ts
@@ -4,10 +4,11 @@ import redisClient from "../config/redisClient";
 
 const verifyTokens = async (headers: Headers) => {
   let newToken = "";
+
+  // Removin the Bearer from headers
   const unverifiedToken = headers.get("x-auth-token")!.startsWith("Bearer")
     ? headers.get("x-auth-token")!.split(" ")[1]
     : "";
-
   const unverifiedRefreshToken = headers
     .get("x-refresh-token")!
     .startsWith("Bearer")
@@ -18,26 +19,25 @@ const verifyTokens = async (headers: Headers) => {
   let verifyToken;
 
   try {
+    // Verify the token
     verifyToken = jwt.verify(unverifiedToken, process.env.JWT_VERIFY_SECRET);
   } catch (_err) {
+    // Process the refresh token if verify token is not valid
     try {
       refreshToken = jwt.decode(unverifiedRefreshToken);
     } catch (err) {
       return { error: "شما اجازه دسترسی ندارید!" };
     }
 
+    // Check if there is the same token in redis or what?
     let redisMembers;
     try {
-      console.time("redis");
-
       await redisClient.connect();
       redisMembers = await redisClient.sIsMember(
         process.env.REDIS_TOKEN_KEY,
         unverifiedRefreshToken
       );
       await redisClient.disconnect();
-
-      console.timeEnd("redis");
     } catch (err) {
       console.log(err);
     }
@@ -46,6 +46,7 @@ const verifyTokens = async (headers: Headers) => {
       return { error: "شما اجازه دسترسی ندارید!" };
     }
 
+    // Making a new token if the check above was true
     newToken = jwt.sign(
       { email: (refreshToken as jwt.JwtPayload).email },
       process.env.JWT_VERIFY_SECRET,
@@ -55,6 +56,7 @@ const verifyTokens = async (headers: Headers) => {
     );
   }
 
+  // Retiurning data from token
   return {
     email: verifyToken
       ? (verifyToken as unknown as jwt.JwtPayload & { email: string }).email
