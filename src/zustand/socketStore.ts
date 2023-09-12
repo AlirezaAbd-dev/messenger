a27e7b72cc9dff71f515540973a643ce8124c8bf
0AliReza0 <alireza.abedi9310@gmail.com>
2023-09-12 22:36:35 +0330

    FIX: Some bug fixed...

--- src/zustand/socketStore.ts
+++ src/zustand/socketStore.ts
@@ -1,21 +1,21 @@
-import { Socket, io } from "socket.io-client";
-import { create } from "zustand";
+import { Socket, io } from 'socket.io-client';
+import { create } from 'zustand';
 import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-} from "../../../socketTypes";
+   ClientToServerEvents,
+   ServerToClientEvents,
+} from '@/socket-server/socketTypes';
 
 interface SocketStore {
-  socket: Socket<ServerToClientEvents, ClientToServerEvents>;
+   socket: Socket<ServerToClientEvents, ClientToServerEvents>;
 }
 
 const useSocket = create<SocketStore>()((set) => ({
-  socket: io("http://localhost:3001", {
-    extraHeaders: {
-      "x-auth-token": localStorage?.getItem("verify-token")!,
-      "x-refresh-token": localStorage?.getItem("refresh-token")!,
-    },
-  }),
+   socket: io('http://localhost:3001', {
+      extraHeaders: {
+         'x-auth-token': localStorage?.getItem('verify-token')!,
+         'x-refresh-token': localStorage?.getItem('refresh-token')!,
+      },
+   }),
 }));
 
 export default useSocket;
