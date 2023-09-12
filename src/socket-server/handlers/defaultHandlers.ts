f58914887e9bdcc95f2faa5f2a73e7a60353a5ee
0AliReza0 <alireza.abedi9310@gmail.com>
2023-09-12 22:04:39 +0330

    FEATURED: defaultHandler added...

--- src/socket-server/handlers/defaultHandlers.ts
+++ src/socket-server/handlers/defaultHandlers.ts
@@ -1,27 +1,30 @@
-import { Users } from "@prisma/client";
-import { ConnectedSocket } from "../types";
+import { STATUS, Users } from '@prisma/client';
+import { ConnectedSocket } from '../types';
+import { prismaClient } from '../server';
 
 const defaultHandlers = async (
-  socket: ConnectedSocket,
-  findUser: Users | undefined,
-  selfId: string,
-  onlineUsers: string[]
+   socket: ConnectedSocket,
+   findUser: Users | undefined,
+   selfId: string,
+   onlineUsers: string[],
 ) => {
-  // Error handling
-  socket.on("error", (err) => {
-    socket.to(selfId).emit("error", err.message);
-  });
+   // Error handling
+   socket.on('error', (err) => {
+      socket.to(selfId).emit('error', err.message);
+   });
 
-  // When user disconnected
-  socket.on("disconnect", async () => {
-    // if (findUser) {
-    //   onlineUsers = onlineUsers.filter((u) => u !== findUser?._id);
-    //   if (findUser.status !== "OFF") {
-    //     findUser.status = "OFFLINE";
-    //     await findUser.save();
-    //   }
-    // }
-  });
+   // When user disconnected
+   socket.on('disconnect', async () => {
+      if (findUser) {
+         onlineUsers = onlineUsers.filter((u) => u !== findUser?.id);
+         if (findUser.status !== 'OFF') {
+            await prismaClient.users.update({
+               where: { id: findUser.id },
+               data: { status: STATUS.OFFLINE },
+            });
+         }
+      }
+   });
 };
 
 export default defaultHandlers;
